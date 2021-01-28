7261072709ed2f4ee824c52561b1bad9e2dc812b
eBenya <mail.ru>
2021-01-28 15:52:32 +0300

    add Identity progect

---
+++ TestASPapp/Controllers/AccountController.cs
@@ -0,0 +1,186 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using TestASP.Models.HelpModel;
+using TestASP.Services;
+
+namespace TestASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, Year = model.Year };
+                // add user
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // set cookie
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager
+                    .PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // checking, allow url to app?
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Wrong login and(or) password");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // delete authentication cookie
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        #region newTest
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId == null || token == null)
+            {
+                return View("Error");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var res = await _userManager.ConfirmEmailAsync(user, token);
+            if (res.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View("Error");
+        }
+        public async Task<IActionResult> RegisterWithConfirm(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email, Year = model.Year };
+                // add user
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account",
+                        new { userId = user.Id, token = token },
+                        protocol: HttpContext.Request.Scheme);
+                    var emailService = new EmailService();
+
+                    await emailService.SendEmailAsync(model.Email, "Confirm your account.",
+                        $"For confirm registration, move to link: <a href=\"{callbackUrl}\">link</a>");
+                    return Content("For registration end, check your email and go to the link");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult LoginWithConfirm(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginWithConfirm(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty, "Email do not confirmed");
+                        return View(model);
+                    }
+                }
+
+                var res = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (res.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email do not confirmed");
+                }
+            }
+
+            return View(model);
+        }
+        #endregion
+    }
+}
---
+++ TestASPapp/Models/HelpModel/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestASP.Models.HelpModel
+{
+    public class User : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Old password")]
+        public string OldPassword { get; set; }
+    }
+}
